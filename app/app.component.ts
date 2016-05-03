d3b8aa643aee30e0fc80d9ea4881a62945d01f9a
Anderson Fernandes <aasf86@gmail.com>
2016-05-03 18:17:03 -0400

    alterando componente

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,6 +1,11 @@
-import {Component} from 'angular2/core'
+import {Component} from 'angular2/core';
+import {Config} from './config.service';
+
 @Component({
     selector: 'my-app',
-    template: '<h1>My Playlist</h1>'
+    template: '<h1 class="jumbotron">{{title}}</h1>'
 })
-export class AppComponent { }
+export class AppComponent 
+{
+    title = Config.TITLE_PAGE;    
+}
