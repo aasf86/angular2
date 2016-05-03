7bfa0424466528892c0c230a9b4bcc32902ad6ea
Anderson Fernandes <aasf86@gmail.com>
2016-05-03 17:31:43 -0400

    preparando primeira aplicaçãao

--- user.js
+++ user.js
@@ -1,12 +1,29 @@
-var User = (function () {
-    function User(firstname, lastname) {
-        this.fullname = firstname + " " + lastname;
+System.register(["./ts_modules/geo"], function(exports_1, context_1) {
+    "use strict";
+    var __moduleName = context_1 && context_1.id;
+    var geo_1;
+    var User, user;
+    return {
+        setters:[
+            function (geo_1_1) {
+                geo_1 = geo_1_1;
+            }],
+        execute: function() {
+            User = (function () {
+                function User(firstname, lastname) {
+                    this.fullname = firstname + " " + lastname;
+                }
+                User.prototype.hello = function () {
+                    var ponto = new geo_1.Point({ x: 0, y: 1 });
+                    return "Hello, " + this.fullname + " -> ponte é 3d: " + ponto.is3d() + ".";
+                    //return "Hello, " + this.fullname+"asdf"; 
+                };
+                return User;
+            }());
+            user = new User("Anderson de Almeida Santos", "Fernandes");
+            //import { Point } from "./ts_modules/geo";
+            document.body.innerHTML = user.hello();
+        }
     }
-    User.prototype.hello = function () {
-        return "Hello, " + this.fullname;
-    };
-    return User;
-}());
-var user = new User("Anderson", "Fernandes");
-document.body.innerHTML = user.hello();
+});
 //# sourceMappingURL=user.js.map
--- user.ts
+++ user.ts
@@ -1,12 +1,16 @@
+import { Point } from "./ts_modules/geo";
+
 class User {
     fullname : string;
     constructor(firstname:string,lastname:string) {
         this.fullname = firstname + " " + lastname;
     }
-    hello():string{
-        return "Hello, " + this.fullname;
+    hello():string{        
+        var ponto:Point = new Point({x:0,y:1});
+        return "Hello, " + this.fullname + " -> ponte é 3d: " + ponto.is3d() + ".";
+        //return "Hello, " + this.fullname+"asdf"; 
     }
 }
-var user = new User("Anderson", "Fernandes");
-
+var user = new User("Anderson de Almeida Santos", "Fernandes");
+//import { Point } from "./ts_modules/geo";
 document.body.innerHTML = user.hello();
